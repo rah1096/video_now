9543c80c5d1a61e46ee9e38ed102465b47b2ed81
Tim <chent1101@gmail.com>
2015-09-05 13:50:03 -0600

    added user auth w/login, logout

--- video_now_app/urls.py
+++ video_now_app/urls.py
@@ -8,7 +8,14 @@ urlpatterns = patterns('',
     #url(r'^pricing/$', TemplateView.as_view(template_name='base.html'), name='home'),
     #url(r'^contact_us/$', TemplateView.as_view(template_name='pricing.html'), name='home'),
     url(r'^$', 'video_now_app.views.home', name='home'),
+    url(r'^staff/$', 'video_now_app.views.staff_home', name='staff'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+)
+
+#auth login/logout
+urlpatterns += patterns('video_now_app.views',
+    url(r'^login/$', 'auth_login', name='login'),
+    url(r'^logout/$', 'auth_logout', name='logout'),
 )
--- video_now_app/views.py
+++ video_now_app/views.py
@@ -1,10 +1,92 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, HttpResponseRedirect
+from django.utils.safestring import mark_safe
+from videos.models import Video
+
+from .forms import LoginForm
 
 
+@login_required
+# @login_required(login_url='/accounts/login/')
 def home(request):
-	name = "Justin"
-	context = {
-		"the_name": name,
-        "number": 34,
-	}
-	return render(request, "home.html", context)
+    if request.user.is_authenticated:
+        print request.user.is_authenticated()
+        name = "Tim"
+        videos = Video.objects.all()
+        embeds = []
+        for vid in videos:
+            code = mark_safe(vid.embed_code)
+            embeds.append("%s" %(code))
+
+        context = {
+            "the_name": name,
+            "number": videos.count(),
+            "videos": videos,
+            "embeds": embeds,
+            "a_code": videos[0].embed_code
+        }
+        return render(request, "home.html", context)
+    #redirect to login
+    else:
+        return HttpResponseRedirect('/login')
+
+@login_required
+# @login_required(login_url='/staff/login/')
+def staff_home(request):
+    context = {
+
+    }
+    return render(request, "home.html", context)
+
+
+
+# def home(request):
+#     if request.user.is_authenticated:
+#         print request.user.is_authenticated()
+#         name = "Tim"
+#         videos = Video.objects.all()
+#         embeds = []
+#         for vid in videos:
+#             code = mark_safe(vid.embed_code)
+#             embeds.append("%s" %(code))
+#
+#         context = {
+#             "the_name": name,
+#             "number": videos.count(),
+#             "videos": videos,
+#             "embeds": embeds,
+#             "a_code": videos[0].embed_code
+#         }
+#         return render(request, "home.html", context)
+#     #redirect to login
+#     else:
+#         return HttpResponseRedirect('/login')
+
+@login_required(login_url='/staff/login')
+def staff_home(request):
+    context = {
+
+    }
+    return render(request, "home.html", context)
+
+def auth_login(request):
+    form = LoginForm(request.POST or None)
+    next_url = request.GET.get('next')
+    if form.is_valid():
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        print username, password
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(next_url)
+
+    context = {"form": form}
+    return render(request, "login.html", context)
+
+def auth_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
