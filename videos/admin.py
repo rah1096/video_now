7c3c347aad5e5ef0f8426abce50dbb7ae35e3d80
Tim <chent1101@gmail.com>
2015-09-06 23:39:21 -0600

    Auto create slug in admin title

--- videos/admin.py
+++ videos/admin.py
@@ -3,7 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from .models import Video, Category
 
-admin.site.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__", "slug"]
+    fields = ['title', 'embed_code',
+              'slug', 'active',
+              'featured', 'free_preview',
+              'category']
+    prepopulated_fields = {'slug': ['title']}
+    class Meta:
+        model = Video
+
+admin.site.register(Video, VideoAdmin)
 
 admin.site.register(Category)
 
