ea5d5cc05e3a8e6fefdf89f265a0a50d84781d37
Tim <chent1101@gmail.com>
2015-09-07 10:51:04 -0600

    Added signals to automate slugs

--- videos/admin.py
+++ videos/admin.py
@@ -5,10 +5,9 @@ from .models import Video, Category
 
 class VideoAdmin(admin.ModelAdmin):
     list_display = ["__unicode__", "slug"]
-    fields = ['title', 'embed_code',
-              'slug', 'active',
-              'featured', 'free_preview',
-              'category']
+    fields = ['title', 'embed_code', 'slug',
+              'active', 'featured',
+              'free_preview', 'category']
     prepopulated_fields = {'slug': ['title']}
     class Meta:
         model = Video
